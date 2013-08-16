49cb6507dedff734c4a49182366a7e150d7f8e64
TylerCuster <tylerjcuster@gmail.com>
2013-08-16 07:36:10 -0400

    clean up

---
+++ questions.js
@@ -0,0 +1,26 @@
+var questions = [
+	{
+		question: "Who is Prime Minister of the United Kingdom?",
+		choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+		correctAnswer:0
+	},
+	{
+		question: "Who is the President of the United States?",
+		choices: ["2 Chainz", "Barack Obama", "An ostrich"],
+		correctAnswer:1
+	},
+	{
+		question: "Who holds the button?",
+		choices: ["It's a cardigan button", "Trevor"],
+		correctAnswer:1
+	},
+	{
+		question: "Here's a freeby",
+		choices: ["Select this to earn a FREE point"],
+		correctAnswer:0
+	},
+	{
+		question: "How many acorns in tree?",
+		choices: ["One or two", "Acorn", "Peanut is a legume", "Acorn", "Demons"],
+		correctAnswer:3
+	}];
