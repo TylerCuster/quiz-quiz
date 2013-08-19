9fc9780e1bc26b3dcd35fa2f6511c6c026428684
TylerCuster <tylerjcuster@gmail.com>
2013-08-19 10:21:38 -0400

    refactor

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
 var app = angular.module('app', []);
 
-app.controller('QuestionCtrl', function($scope) {
+app.controller('QuestionController', function($scope) {
 	$scope.questions = questions;
-	$scope.questionsCount = questions.length;
 	$scope.currentQuestion = 0;
 
 	$scope.nextQuestion = function(questionIndex, userAnswer) {
 		$scope.questions[questionIndex].userAnswer = userAnswer;
 		$scope.currentQuestion += 1;
-		$scope.lastQuestion = ($scope.currentQuestion == $scope.questionsCount);
-		if ($scope.lastQuestion) {
-			$scope.score();
-		}
-	};
-
-	$scope.score = function() {
-		var score = 0;
-		for (var i = 0; i < $scope.questionsCount; i++) {
-			var question = $scope.questions[i];
-			if (question.userAnswer == question.correctAnswer) {
-				score += 1;
+		$scope.lastQuestion = ($scope.currentQuestion == questions.length);
+		if ($scope.lastQuestion) { 
+			var score = 0;
+			for (var i = 0; i < questions.length; i++) {
+				var question = $scope.questions[i];
+				if (question.userAnswer == question.correctAnswer) { score += 1; }
 			}
+			$scope.score = score;
 		}
-		$scope.score = score;
 	};
 });
