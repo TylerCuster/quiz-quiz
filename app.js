2643f44a7584fe6499b428dca52156f45bd9674c
TylerCuster <tylerjcuster@gmail.com>
2013-08-21 21:04:19 -0400

    back button, styling

--- app.js
+++ app.js
@@ -2,21 +2,41 @@ var app = angular.module('app', []);
 
 app.controller('QuestionController', function($scope) {
 	$scope.questions = questions;
-	$scope.currentQuestion = 0;
+	$scope.quiz = {currentQuestion: 0};
+
+	$scope.showQuestion = function(index) {
+		return $scope.quiz.currentQuestion == index;
+	};
+
+	$scope.firstQuestion = function(index) {
+		return $scope.quiz.currentQuestion == 0;
+	};
+
+	$scope.previousQuestion = function () {
+		$scope.quiz.currentQuestion -= 1;
+	};
 
 	$scope.nextQuestion = function(index, userAnswer) {
+		logAnswer(index, userAnswer);
+		$scope.quiz.currentQuestion += 1;
+		$scope.quiz.lastQuestion = ($scope.quiz.currentQuestion == questions.length);
+		if ($scope.quiz.lastQuestion) { score (); }
+	};
+
+	var logAnswer = function (index, userAnswer) {
 		$scope.questions[index].userAnswer = userAnswer;
-		$scope.currentQuestion += 1;
-		$scope.lastQuestion = ($scope.currentQuestion == questions.length);
-		if ($scope.lastQuestion) { $scope.score(); }
-	}
+	};
 	
-	$scope.score = function() {
+	var score = function () {
 		var score = 0;
 		for (var i = 0; i < questions.length; i++) {
 			var question = $scope.questions[i];
 			if (question.userAnswer == question.correctAnswer) { score += 1; }
 		}
-		$scope.score = score;
+		$scope.quiz.score = score;
+	};
+
+	$scope.reload = function () {
+		window.location.reload();
 	};
 });
