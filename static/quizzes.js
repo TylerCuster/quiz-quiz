1794eaf613e6d37f80ee5f28729aca5dd5d04d61
TylerCuster <tylerjcuster@gmail.com>
2013-09-02 19:20:45 -0400

    re-organized, added 4th quiz

--- static/quizzes.js
+++ static/quizzes.js
@@ -1,5 +1,5 @@
-[
-	{
+var quizzes = [
+	{ 
 		quizId: 1,
 		quiz: [
 			{
@@ -33,12 +33,42 @@
 		quizId: 2,
 		quiz: [
 			{
-				question: "Yump",
+				question: "Who is holding a small dog at this very moment?",
 				choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
 				correctAnswer:0
 			},
 			{
-				question: "Hole?",
+				question: "Who has been the lowest below sea level?",
+				choices: ["2 Chainz", "Barack Obama", "An ostrich"],
+				correctAnswer:1
+			},
+			{
+				question: "Who holds the button?",
+				choices: ["It's a cardigan button", "Lue"],
+				correctAnswer:1
+			},
+			{
+				question: "Here's a freeby",
+				choices: ["Select this to earn a FREE point"],
+				correctAnswer:0
+			},
+			{
+				question: "How many acorns in tree with three acorns, twelve leaves, twenty-five branches, sixty-five legumes?",
+				choices: ["One or two", "Acorn", "Peanut is a legume", "Acorns", "Demons"],
+				correctAnswer:3
+			}
+		]
+	},
+	{ 
+		quizId: 3,
+		quiz: [
+			{
+				question: "Shump?",
+				choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+				correctAnswer:0
+			},
+			{
+				question: "Who is the President of the United States?",
 				choices: ["2 Chainz", "Barack Obama", "An ostrich"],
 				correctAnswer:1
 			},
@@ -60,15 +90,15 @@
 		]
 	},
 	{ 
-		quizId: 3,
+		quizId: 4,
 		quiz: [
 			{
-				question: "Shump",
-				choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+				question: "What's a better cat name?",
+				choices: ["Lumpy Lue", "Walter", "Simple Sally", "JJ Juicy"], 
 				correctAnswer:0
 			},
 			{
-				question: "Who is the President of the United States?",
+				question: "Who is not the President of the United States?",
 				choices: ["2 Chainz", "Barack Obama", "An ostrich"],
 				correctAnswer:1
 			},
