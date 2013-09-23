1587348a173a8ab727221b4d9565a4f83da38e7a
tyler <tyler@ubuntu.(none)>
2013-09-23 03:04:14 -0400

    unit testing with karma and e2e testing configured

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -8,37 +8,20 @@ describe('my app', function() {
     browser().navigateTo('../../app/index.html');
   });
 
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
+  it('should automatically go to index when location hash/fragment is empty', function() {
+    expect(browser().location().url()).toBe("/index.html");
   });
 
 
-  describe('view1', function() {
+  describe('quiz 1', function() {
 
     beforeEach(function() {
-      browser().navigateTo('#/view1');
+      browser().navigateTo('#/quiz/1');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
+    it('should render quiz 1 when user navigates to /quiz/1', function() {
+      expect(browser().location().url()).toBe("/quiz/1");
     });
 
   });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,8 +3,11 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+  beforeEach(angular.mock.module('myApp.controllers'));
 
+  it('should have a QuizController controller', function() {
+  	1 + 2 == 3;
+  });
 
   it('should ....', inject(function() {
     //spec body
