f009461cc8b14b4acaed62d3ab5a26b61a0178e6
TylerCuster <TylerJCuster@gmail.com>
2013-10-24 15:22:13 -0400

    delete nganimate

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-window.app = angular.module('quizquiz', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', 'ngAnimate', 'quizquiz.controllers', 'quizquiz.directives', 'quizquiz.services']);
+window.app = angular.module('quizquiz', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', 'quizquiz.controllers', 'quizquiz.directives', 'quizquiz.services']);
 
 // bundling dependencies
 window.angular.module('quizquiz.controllers', ['quizquiz.controllers.header', 'quizquiz.controllers.index', 'quizquiz.controllers.create', 'quizquiz.controllers.list', 'quizquiz.controllers.quiz']);
